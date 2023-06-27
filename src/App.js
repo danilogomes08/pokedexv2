1be617d4a13b45aa02993717e494f83a7cccd3c4
Danilo Gomes <danilo.dsgomes@gmail.com>
2023-06-27 19:42:25 -0300

    estilizado cards e types conforme cada tipo do pokemon

--- src/App.js
+++ src/App.js
@@ -11,58 +11,39 @@ function App() {
   const limit = 20;
 
   const getApiData = async () => {
-      const pokeIds = [];
-      for (var i = count + 1; i <= limit + count; i++) {
-        pokeIds.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-      }
-      await Promise.all(pokeIds.map((pokeId) => fetch(pokeId)))
-        .then((res) => Promise.all(res.map(async r => r.json())))
-        .then((res) => {
-          setApi([...api, ...res]);
-        });
+    const pokeIds = [];
+    for (var i = count + 1; i <= limit + count; i++) {
+      pokeIds.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
+    await Promise.all(pokeIds.map((pokeId) => fetch(pokeId)))
+      .then((res) => Promise.all(res.map(async r => r.json())))
+      .then((res) => {
+        setApi([...api, ...res]);
+      });
+  }
 
   const more = () => {
     setCount(count + limit)
+
   }
 
   const previus = () => {
     setCount(count - limit)
-    setApi(api.splice(0, 20))
+    setApi(api.splice(0, limit))
   }
 
   useEffect(() => {
     getApiData();
   }, [count]);
 
-    const typePokemon = () => {
-      if (api.type === "fairy") return "Fairy"
-      if (api.type === "steel") return "Steel"
-      if (api.type === "dark") return "Dark"
-      if (api.type === "dragon") return "Dragon"
-      if (api.type === "ghost") return "Ghost"
-      if (api.type === "rock") return "Rock"
-      if (api.type === "bug") return "Bug"
-      if (api.type === "psychic") return "Psychic"
-      if (api.type === "ground") return "Ground"
-      if (api.type === "poison") return "Poison"
-      if (api.type === "fighting") return "Fighting"
-      if (api.type === "ice") return "Ice"
-      if (api.type === "grass") return "Grass"
-      if (api.type === "electric") return "Electric"
-      if (api.type === "normal") return "Normal"
-      if (api.type === "water") return "Water"
-      if (api.type === "fire") return "Fire"
-  
-      return ""
-    }
+
   
 
   return (
     <div className="App">
         <h1> POKEDEX V2</h1>
         <div className="Cards">
-          {api.map((item) => (
+          {api.map((item, index) => (
             <Cards 
               name={item.name}
               img={item.sprites.versions['generation-v']['black-white'].animated.front_default 
@@ -70,7 +51,7 @@ function App() {
               : item.sprites.front_default}
               id={item.id}
               type={item.types}
-              typePokemon={typePokemon()}
+              key={index}
             />
           ))}
         </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,23 +3,72 @@ import './Cards.css'
 
 const Cards = props => {
 
-    const types = () => {
-		if (props.type[1]) {
-			return props.type[0].type.name + " | " + props.type[1].type.name;
-		}
-		else {
-			return props.type[0].type.name;
-		}
-	}
+
+    const typePokemon1 = () => {
+        
+        if (props.type[0].type.name === "fairy") return "Fairy"
+        if (props.type[0].type.name === "steel") return "Steel"
+        if (props.type[0].type.name === "dark") return "Dark"
+        if (props.type[0].type.name === "dragon") return "Dragon"
+        if (props.type[0].type.name === "ghost") return "Ghost"
+        if (props.type[0].type.name === "rock") return "Rock"
+        if (props.type[0].type.name === "bug") return "Bug"
+        if (props.type[0].type.name === "psychic") return "Psychic"
+        if (props.type[0].type.name === "ground") return "Ground"
+        if (props.type[0].type.name === "poison") return "Poison"
+        if (props.type[0].type.name === "fighting") return "Fighting"
+        if (props.type[0].type.name === "ice") return "Ice"
+        if (props.type[0].type.name === "grass") return "Grass"
+        if (props.type[0].type.name === "electric") return "Electric"
+        if (props.type[0].type.name === "normal") return "Normal"
+        if (props.type[0].type.name === "water") return "Water"
+        if (props.type[0].type.name === "fire") return "Fire"
+        if (props.type[0].type.name === "flying") return "Flying"
+    
+        return ""
+      }
+
+      const typePokemon2 = () => {
+        
+        if (props.type[1].type.name === "fairy") return "Fairy"
+        if (props.type[1].type.name === "steel") return "Steel"
+        if (props.type[1].type.name === "dark") return "Dark"
+        if (props.type[1].type.name === "dragon") return "Dragon"
+        if (props.type[1].type.name === "ghost") return "Ghost"
+        if (props.type[1].type.name === "rock") return "Rock"
+        if (props.type[1].type.name === "bug") return "Bug"
+        if (props.type[1].type.name === "psychic") return "Psychic"
+        if (props.type[1].type.name === "ground") return "Ground"
+        if (props.type[1].type.name === "poison") return "Poison"
+        if (props.type[1].type.name === "fighting") return "Fighting"
+        if (props.type[1].type.name === "ice") return "Ice"
+        if (props.type[1].type.name === "grass") return "Grass"
+        if (props.type[1].type.name === "electric") return "Electric"
+        if (props.type[1].type.name === "normal") return "Normal"
+        if (props.type[1].type.name === "water") return "Water"
+        if (props.type[1].type.name === "fire") return "Fire"
+        if (props.type[1].type.name === "flying") return "Flying"
+    
+        return ""
+      }
 
     return (
-        <div className={`Card ${props.typePokemon}`}>
+        <div className={`Card ${typePokemon1()}`}>
             <p>#{props.id}</p>
             <div>
                 <img src={props.img} />
             </div>
             <p> {props.name} </p>
-            {types()}
+            {props.type[1] ? 
+                <div className="Type">
+                    <p className={typePokemon1()}> {props.type[0].type.name} </p>
+                    <p className={typePokemon2()}> {props.type[1].type.name} </p>
+                </div>
+                :
+                <div className="Type">
+                    <p className={typePokemon1()}>{props.type[0].type.name} </p>
+                </div>
+            }
         </div>
     )
 }
